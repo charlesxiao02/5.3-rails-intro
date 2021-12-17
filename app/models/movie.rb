30779591bb60c5c6c2cd774eb3d7980443b64ff0
Charles Xiao <cxiao02@berkeley.edu>
2021-12-17 20:53:40 +0000

    Trying just html

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,11 +1,11 @@
 class Movie < ActiveRecord::Base
   
-  def all_ratings
-    return ['G', 'PG', 'PG-13', 'R']
-  end
+  #def all_ratings
+  #  return ['G', 'PG', 'PG-13', 'R']
+  #end
   
-  def ratings_to_show
-    return nil
-  end
+  #def ratings_to_show
+  #  return nil
+  #end
   
 end
